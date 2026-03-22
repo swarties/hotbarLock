2a8da75bab2b271eca02ca9bd4b189b1f9d93027
swarties <76826079+swarties@users.noreply.github.com>
2026-03-21 21:17:41 -0400

    Initial Fabric/IDE Setup

---
+++ src/client/java/com/roks/lok/HotbarLockClient.java
@@ -0,0 +1,10 @@
+package com.roks.lok;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HotbarLockClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
